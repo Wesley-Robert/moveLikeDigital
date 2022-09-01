a0e8ec6c69196239720ee4ef5d410fa08a77d5a2
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2022-09-01 20:37:36 -0300

    kjfuyf

--- main.js
+++ main.js
@@ -32,18 +32,44 @@ document.querySelectorAll('[all-nav]').forEach(link => {
 
         const container = document.querySelector('body')
         
-        fetch(link.getAttribute('all-nav'))
-            .then(resp => resp.text())
-            .then(html => container.innerHTML = html)
+        fetch(link).then(function(response){
+            if(response.ok){
+                link.getAttribute('all-nav')
+                    .then(resp => resp.text())
+                    .then(html => container.innerHTML = html)
+                scripts()  
+            }else{
+                console.log('Sucesso na conexão')
+            }
+        })
+            
+
     }
 });
 
 
 const scripts = function(){
-    let img = document.querySelectorAll('.clients img')
+    let img = document.querySelectorAll('.clients img').forEach(function(img){
         img.onclick = function(){
             img.classList.toggle('noPreview')
-            let txtP = document.querySelector('.clients .p-tx')
+            let txtP = document.querySelectorAll('.clients .p-tx').forEach(function(txtP){
                 txtP.classList.toggle('previewText')
+            })    
         }  
+    })
 }
+
+let myImage = document.querySelector('.img-bx img');
+
+fetch('MoveLike oficial.jpg').then(function(response){
+    if(response.ok){
+        response.blob().then(function(myBlob){
+            let objectURL = URL.createObjectURL(myBlob)
+            myImage.src = objectURL
+        })
+    }else{
+        console.log('Network response was not ok')
+    }
+}).catch(function(error){
+    console.log('There has been a problem with your fetch operation: ' + error.message)
+})
